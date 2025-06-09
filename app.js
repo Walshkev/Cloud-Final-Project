15c645ba3b7e1d4f8a257466c4f609916a65a191
Autoro <alexdmiesbauer@gmail.com>
2025-06-09 13:56:56 -0700

    Added rate limiting to api.

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 require("dotenv").config();
+const yn = require("yn").default;
 const express = require("express");
 const logger = require("morgan");
 const { connectDb } = require("./lib/mongo");
 const { connectRedis } = require("./lib/redis");
 const { authenticate } = require("./middleware/auth");
+const { rateLimit } = require("./middleware/rateLimit");
 
 const port = process.env.PORT;
+const rateLimitEnabled = yn(process.env.RATE_LIMIT_ENABLED);
 const app = express();
 
-connectDb()
-    .then(connectRedis())
-    .then(app.listen(port, () =>
-        console.log(`=== Server listening on port ${port}`))
-    );
+const promise = connectDb();
+rateLimitEnabled && promise.then(() => connectRedis());
+promise.then(() =>
+    app.listen(port, () =>
+        console.log(`=== Server is running on port ${port}`)
+    )
+);
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(authenticate);
+rateLimitEnabled && app.use(rateLimit);
 app.use("/", require("./routes/index"));
